08606aee7b97a5dfc6bf9b70fddd6af545a5f220
Uma Desai <umadesai38@gmail.com>
2018-07-11 18:05:14 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -26,12 +26,16 @@ end
 puts last_student_by_index
 
 def first_student_by_method
+  puts STUDENT_NAMES.first
 end
+puts first_student_by_method
 
 def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
+puts STUDENT_NAMES.last
 end
+puts last_student_by_method
 
 def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
+ puts STUDENT_NAMES
 end
+puts first_second_and_third_students
